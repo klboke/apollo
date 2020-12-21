6ecfbe63ef708989bbc5c522f6a8cc411df53e49
kl <chenkailing@xd.com>
2020-12-21 17:59:45 +0800

    Fix UnnecessaryStubbingException thrown by NacosDiscoveryServiceTest

--- apollo-configservice/src/test/java/com/ctrip/framework/apollo/metaservice/service/NacosDiscoveryServiceTest.java
+++ apollo-configservice/src/test/java/com/ctrip/framework/apollo/metaservice/service/NacosDiscoveryServiceTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -54,22 +55,21 @@ public class NacosDiscoveryServiceTest {
         String someIp = "1.2.3.4";
         int somePort = 8080;
         String someInstanceId = "someInstanceId";
-        Instance someServiceInstance = mockServiceInstance(someServiceId, someInstanceId,
-                someIp,somePort);
+        Instance someServiceInstance = mockServiceInstance(someInstanceId, someIp, somePort);
 
-        when(nacosNamingService.selectInstances(someServiceId,true)).thenReturn(
+        when(nacosNamingService.selectInstances(someServiceId, true)).thenReturn(
                 Lists.newArrayList(someServiceInstance));
 
         List<ServiceDTO> serviceDTOList = nacosDiscoveryService.getServiceInstances(someServiceId);
-
+        ServiceDTO serviceDTO = serviceDTOList.get(0);
         assertEquals(1, serviceDTOList.size());
-        assertEquals(serviceDTOList.get(0).getHomepageUrl(),"http://1.2.3.4:8080/");
+        assertEquals(someServiceId, serviceDTO.getAppName());
+        assertEquals("http://1.2.3.4:8080/", serviceDTO.getHomepageUrl());
 
     }
 
-    private Instance mockServiceInstance(String serviceId, String instanceId, String ip, int port) {
+    private Instance mockServiceInstance(String instanceId, String ip, int port) {
         Instance serviceInstance = mock(Instance.class);
-        when(serviceInstance.getServiceName()).thenReturn(serviceId);
         when(serviceInstance.getInstanceId()).thenReturn(instanceId);
         when(serviceInstance.getIp()).thenReturn(ip);
         when(serviceInstance.getPort()).thenReturn(port);
