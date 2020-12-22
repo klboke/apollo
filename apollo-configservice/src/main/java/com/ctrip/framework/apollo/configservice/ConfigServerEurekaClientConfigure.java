4dda7109c0bfe9d388f11fa743c100afaec111ba
chenkailing <632104866@qq.com>
2020-12-22 23:59:19 +0800

    Adjust the refresh timing of eurekaClient to EurekaInstanceRegisteredEvent

--- apollo-configservice/src/main/java/com/ctrip/framework/apollo/configservice/ConfigServerEurekaClientConfigure.java
+++ apollo-configservice/src/main/java/com/ctrip/framework/apollo/configservice/ConfigServerEurekaClientConfigure.java
@@ -1,22 +1,26 @@
 package com.ctrip.framework.apollo.configservice;
 
 import com.ctrip.framework.apollo.biz.eureka.ApolloEurekaClientConfig;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
+import com.netflix.appinfo.InstanceInfo;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.context.scope.refresh.RefreshScope;
+import org.springframework.cloud.netflix.eureka.server.event.EurekaInstanceRegisteredEvent;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * @author : kl
  * After startup, set FetchRegistry to true, refresh eureka client
  **/
-@ConditionalOnProperty(value = "eureka.client.enabled",havingValue = "true",  matchIfMissing = true)
 @Configuration
-public class ConfigServerEurekaClientConfigure implements ApplicationRunner {
+@ConditionalOnProperty(value = "eureka.client.enabled", havingValue = "true", matchIfMissing = true)
+public class ConfigServerEurekaClientConfigure {
 
     private static final String EUREKACLIENT_BEANNAME = "eurekaClient";
     private final ApolloEurekaClientConfig eurekaClientConfig;
+    private final AtomicBoolean isRefresh = new AtomicBoolean(false);
     private final RefreshScope refreshScope;
 
     public ConfigServerEurekaClientConfigure(ApolloEurekaClientConfig eurekaClientConfig, RefreshScope refreshScope) {
@@ -24,13 +28,18 @@ public class ConfigServerEurekaClientConfigure implements ApplicationRunner {
         this.refreshScope = refreshScope;
     }
 
-    @Override
-    public void run(ApplicationArguments args) {
-        this.refreshEurekaClient();
+    @EventListener
+    public void listenEurekaInstanceRegisteredEvent(EurekaInstanceRegisteredEvent event) {
+        InstanceInfo.InstanceStatus status = event.getInstanceInfo().getStatus();
+        if (InstanceInfo.InstanceStatus.UP.equals(status) && !eurekaClientConfig.isFetchRegistry()) {
+            this.refreshEurekaClient();
+        }
     }
 
-    private void refreshEurekaClient(){
-        eurekaClientConfig.setFetchRegistry(true);
-        refreshScope.refresh(EUREKACLIENT_BEANNAME);
+    private void refreshEurekaClient() {
+        if (isRefresh.compareAndSet(false, true)) {
+            eurekaClientConfig.setFetchRegistry(true);
+            refreshScope.refresh(EUREKACLIENT_BEANNAME);
+        }
     }
 }
